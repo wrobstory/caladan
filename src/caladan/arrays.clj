bc25b59d9aa627a38e788085b96cf9856063cc8f
Rob Story <wrobstory@gmail.com>
2014-11-10 17:15:22 -0800

    ENH: Update Arrays, add subsetting and index filtering

--- src/caladan/arrays.clj
+++ src/caladan/arrays.clj
@@ -1,32 +1,54 @@
 (ns caladan.arrays
-  (:require [hiphip.long :as hhl]))
+  (:require [hiphip.long :as hhl])
+  (:refer-clojure :exclude [filter])
+  (:import (java.util HashSet)))
 
-(defprotocol Array
-  (takes [this]))
+(defn subset
+  [^longs indices hashed-indices levels]
+    (let [return-vector (transient [])]
+      (hhl/doarr [i indices]
+        (if (.contains hashed-indices i)
+          (conj! return-vector (get levels i))))
+      (persistent! return-vector)))
 
-(defrecord CategoricalArray
-  [levels
-   ^longs indices])
+(defn filter-level-indices
+  [pred levels]
+    (let [hashed-levels (HashSet.)
+          level-filter (fn [hl item]
+                          (if (pred (get item 1))
+                            (do
+                              (.add hashed-levels (get item 0))
+                              hashed-levels)
+                            hashed-levels))]
+      (reduce level-filter hashed-levels (map-indexed vector levels))
+      hashed-levels))
 
-(defn get-categorical-indices
-  [str-seq levels]
-    (let [values (make-array Long/TYPE (count str-seq))]
-      (doseq [[i value] (map-indexed vector str-seq)]
+(defn get-indexed-indices
+  [in-seq levels]
+    (let [values (make-array Long/TYPE (count in-seq))]
+      (doseq [[i value] (map-indexed vector in-seq)]
         (aset ^longs values i (long (.indexOf levels ^String value))))
       values))
 
-(defn make-categorical-array
+(defprotocol Array
+  (takes [this length])
+  (filter [this pred]))
+
+(defrecord IndexedArray [levels ^longs indices]
+  Array
+  (takes [this length]
+    (let [return-vector (transient [])
+          levels (:levels this)
+          indices (:indices this)]
+      (hhl/doarr [x indices]
+        (conj! return-vector (get levels x)))
+      (vec (persistent! return-vector))))
+  (filter [this pred]
+    (let [filtered-indices (filter-level-indices pred (:levels this))]
+      (subset (:indices this) filtered-indices (:levels this)))))
+
+(defn make-indexed-array
   [str-seq]
     (let [levels (into [] (apply sorted-set str-seq))
-          indices (get-categorical-indices str-seq levels)]
-      (CategoricalArray. levels indices)))
-
-(extend-protocol Array
-  CategoricalArray
-    (takes [this length]
-      (let [return-vector (transient [])
-            levels (:levels this)
-            indices (:indices this)]
-        (hhl/doarr [x indices]
-          (conj! return-vector (get levels x)))
-        (vec (persistent! return-vector)))))
+          indices (get-indexed-indices str-seq levels)]
+      (IndexedArray. levels indices)))
